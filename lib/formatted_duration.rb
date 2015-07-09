885d7f4588aed3c53eccb434c89a1cbe95fb7065
Richard Venneman <richardvenneman@me.com>
2015-07-09 14:43:10 +0200

    Refactor formatting code to reduce complexity and duplication, increase gem version to 1.1.0

--- lib/formatted_duration.rb
+++ lib/formatted_duration.rb
@@ -2,44 +2,42 @@ require 'ruby-duration'
 
 class FormattedDuration
   CONSTRAINTS = [:minutes, :hours, :days, :weeks]
+  FORMATS = { weeks: '%w %~w', days: '%tdu', hours: '%thu', minutes: '%tmu' }
+  BARE_FORMATS = { days: '%d %~d', hours: '%h %~h', minutes: '%m %~m' }
+  BARE = { days: 7, hours: 24, minutes: 60 }
 
   def initialize(minutes, constraint = :weeks)
     @constraint = CONSTRAINTS.index(constraint)
-
     @minutes = minutes
     @hours = minutes / 60
     @days = @hours / 24
     @weeks = @days / 7
+    @output = []
   end
 
   def format
-    weekless_days = @days % 7
-    dayless_hours = @hours % 24
-    hourless_minutes = @minutes % 60
-    output = []
+    append(:weeks, @weeks, '%w %~w') if @weeks > 0 && @constraint > 2
+    add(:days, @days, 2)
+    add(:hours, @hours, 1)
+    add(:minutes, @minutes, 0)
 
-    if @weeks != 0 && @constraint > 2
-      output << Duration.new(weeks: @weeks).format('%w %~w')
+    @output.join(', ')
+  end
+
+  def append(unit, value, format)
+    opts = {}
+    opts[unit] = value
+
+    @output << Duration.new(opts).format(format)
+  end
+
+  def add(unit, value, constraint)
+    bare = value % BARE[unit]
+
+    if value > 0 && @constraint == constraint
+      append(unit, value, FORMATS[unit])
+    elsif bare > 0 && @constraint > constraint - 1
+      append(unit, bare, BARE_FORMATS[unit])
     end
-
-    if @days > 0 && @constraint == 2
-      output << Duration.new(days: @days).format('%tdu')
-    elsif weekless_days != 0 && @constraint > 1
-      output << Duration.new(days: weekless_days).format('%d %~d')
-    end
-
-    if @hours > 0 && @constraint == 1
-      output << Duration.new(hours: @hours).format('%thu')
-    elsif dayless_hours != 0 && @constraint > 0
-      output << Duration.new(hours: dayless_hours).format('%h %~h')
-    end
-
-    if @minutes > 0 && @constraint == 0
-      output << Duration.new(minutes: @minutes).format('%tmu')
-    elsif hourless_minutes != 0
-      output << Duration.new(minutes: hourless_minutes).format('%m %~m')
-    end
-
-    output.join(', ')
   end
 end
