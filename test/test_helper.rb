162f346204a1c7a7bfda05960cf5d75b27d9de64
Richard Venneman <richardvenneman@me.com>
2015-07-09 14:39:44 +0200

    [ci skip] Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 # Coverage
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+elsif ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Libraries', 'lib'
+  end
+end
 
 # Source files
 require './lib/formatted_duration'
